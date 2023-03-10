763f972b0193f113e766d87f942ce566513207b5
Neha <neha.thakran2001@gmail.com>
2023-03-10 14:35:36 +0530

    added message in add.java

--- add.java
+++ add.java
@@ -2,6 +2,6 @@ public class add {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        System.out.println(a + b);
+        System.out.println("Sum is " + (a + b));
     }
 }
